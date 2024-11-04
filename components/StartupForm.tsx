dc7f2e7f51e947068ac2262aad2fd8beb8614bf5
Prashant Yadav <pygold@gmail.com>
2024-11-04 22:28:07 +0530

    Server action to save startup pitch

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -8,13 +8,14 @@ import { Button } from "./ui/button";
 import { formSchema } from "@/lib/validations";
 import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
   const [pitch, setPitch] = useState("");
   const { toast } = useToast();
-  // const router = useRouter();
+  const router = useRouter();
 
   const handleFormSubmit = async (prevState: any, formData: FormData) => {
     try {
@@ -30,6 +31,19 @@ const StartupForm = () => {
 
       console.log("formValues");
       console.log(formValues);
+
+      const result = await createPitch(prevState, formData, pitch);
+
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created successfully",
+        });
+
+        router.push(`/startup/${result._id}`);
+      }
+
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErrors = error.flatten().fieldErrors;
